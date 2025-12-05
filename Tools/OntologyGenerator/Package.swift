72c0f5c02e06d63c34ee2bcb9c858a0c19b632d3
johgu739 <johan123gunnarsson@gmail.com>
2025-12-05 07:42:01 +0000

    STEP 4: Remove header from Package.swift - tools-version must be first line

--- Tools/OntologyGenerator/Package.swift
+++ Tools/OntologyGenerator/Package.swift
@@ -1,16 +1,3 @@
-// @EntelechiaHeaderStart
-// Signifier: Package
-// Substance: TODO_AGENT_FILL
-// Genus: TODO_AGENT_FILL
-// Differentia: TODO_AGENT_FILL
-// Form: TODO_AGENT_FILL
-// Matter: TODO_AGENT_FILL
-// Powers: TODO_AGENT_FILL
-// FinalCause: TODO_AGENT_FILL
-// Relations: TODO_AGENT_FILL
-// CausalityType: TODO_AGENT_FILL
-// @EntelechiaHeaderEnd
-
 // swift-tools-version: 5.9
 import PackageDescription
 
